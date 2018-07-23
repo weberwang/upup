d802fed5886cdab20d532381e49160ca522f7702
Minko <for.js221@gmail.com>
2018-07-23 23:45:00 +0800

    修复相机反向移动跟随异常

--- assets/scripts/easyCamera.ts
+++ assets/scripts/easyCamera.ts
@@ -13,33 +13,52 @@ const { ccclass, property } = cc._decorator;
 @ccclass
 export default class NewClass extends cc.Component {
 
-    @property({ type: cc.Vec2, tooltip: "目标显示对象移动偏移量超过指定大小，相机开始移动,负值忽略" })
+    @property({ tooltip: "目标显示对象移动相对相机偏移量超过指定大小，相机开始移动,负值忽略" })
     offset: cc.Vec2 = cc.v2();
 
     @property({ type: cc.Node, tooltip: "目标显示对象" })
     target: cc.Node = null;
 
-    private curPosition: cc.Vec2 = null;
+    @property({ type: cc.Component.EventHandler, tooltip: "相机移动触发的事件" })
+    cameraUpdateEvents: cc.Component.EventHandler[] = [];
+
 
     resetInEditor() {
         if (!this.getComponent(cc.Camera)) {
             this.enabled = false;
         } else {
-            cc.log("加载相机")
+            cc.log("加载相机");
         }
     }
 
-    onLoad() {
-        this.curPosition = this.target.position;
+    start() {
     }
 
     update() {
-        if (this.offset.x >= 0 && Math.abs(this.target.x - this.curPosition.x) > this.offset.x) {
-                
+        var targetWorldPoint = this.target.convertToWorldSpaceAR(cc.Vec2.ZERO);
+        var cameraPoint = this.node.convertToWorldSpaceAR(cc.Vec2.ZERO);
+        let move = false;
+        if (this.offset.x >= 0 && Math.abs(targetWorldPoint.x - cameraPoint.x) > this.offset.x) {
+            if (targetWorldPoint.x > cameraPoint.x) {
+                this.node.x += targetWorldPoint.x - cameraPoint.x - this.offset.x;
+            } else {
+                this.node.x += targetWorldPoint.x - cameraPoint.x + this.offset.x;
+            }
+            move = true;
         }
 
-        if (this.offset.y >= 0 && Math.abs(this.target.y - this.curPosition.y) > this.offset.y) {
-
+        if (this.offset.y >= 0 && Math.abs(targetWorldPoint.y - cameraPoint.y) > this.offset.y) {
+            if (targetWorldPoint.y > cameraPoint.y) {
+                this.node.y += targetWorldPoint.y - cameraPoint.y - this.offset.y;
+            } else {
+                this.node.y += targetWorldPoint.y - cameraPoint.y + this.offset.y;
+            }
+            move = true;
+        }
+        if (move) {
+            this.cameraUpdateEvents.forEach((handler:cc.Component.EventHandler)=>{
+                handler.emit([this.node]);
+            })
         }
     }
 }
